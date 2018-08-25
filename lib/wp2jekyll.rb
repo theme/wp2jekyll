80c382c603c182a2b6d620afb11b97f2d2dc7553
theme <xenotheme@gmail.com>
2018-08-25 13:06:07 +0800

    Add design comments.

--- lib/wp2jekyll.rb
+++ lib/wp2jekyll.rb
@@ -1,5 +1,20 @@
 require "wp2jekyll/version"
 
+# This gem try to import 2 kinds of things, into jekyll blog:
+
+# 1. other blog's posts.
+# 1.1 TODO get input_dirs ( user config search path, and dir name wildcard)
+# 1.2 check if post already exists in jekyll/_posts/ for each post in each input_dir (using similarity algorithm)
+# 1.3 hint user to modify post name, then write it into jekyll/_posts/ dir.
+# 1.4 patch post content (like html tags) to jekyll supported markdown format. (TODO some old format is lost)
+
+
+# 2. TODO other blog's images.
+# 2.1 get input_dirs (scan input dir_list, search for known_names, such as wordpress's "wp-content/uploads/")
+# 2.2 check if image file is already exist in jekyll's _assets dir.
+# 2.3 copy image, keep dir structure for same kind of source. (wordpress | wordpress.com | blogspot)
+# 2.4 modify every link that referes to a image in jekyll/_posts/ to new dir path.
+
 module Wp2jekyll
   require 'wp2jekyll/wp_patch'
   require 'wp2jekyll/post_merge'
