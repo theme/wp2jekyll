15214aee6164298637c1b18f15fb35e0682d1287
theme <xenotheme@gmail.com>
2018-09-13 22:31:57 +0800

    Stale some test.

--- test/wp_merge_test.rb
+++ test/wp_merge_test.rb
@@ -10,16 +10,16 @@ class TestMergerMarkdownFile < MiniTest::Test
     Post.new(File.expand_path('../sample/post.md', __FILE__)).hint_contents
   end
 
-  def test_diff
-    fa = File.expand_path('../sample/post.md', __FILE__)
-    fb = File.expand_path('../sample/post 2.md', __FILE__)
+  # def test_diff
+  #   fa = File.expand_path('../sample/post.md', __FILE__)
+  #   fb = File.expand_path('../sample/post 2.md', __FILE__)
     
-    begin
-      assert(PostCompare.new(fa, fb).similar?)
-    rescue UncertainSimilarityError
-      nil
-    end
+  #   begin
+  #     assert(PostCompare.new(fa, fb).similar?)
+  #   rescue UncertainSimilarityError
+  #     nil
+  #   end
 
-  end
+  # end
 
 end
